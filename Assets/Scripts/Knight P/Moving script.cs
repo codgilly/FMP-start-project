aa47cfeaef88828b6d4fc3b760ad7e94f942d016
Cody gilbert <146819086+codgilly@users.noreply.github.com>
2024-04-16 16:09:12 +0100

    bugs!

--- Assets/Scripts/Knight P/Moving script.cs
+++ Assets/Scripts/Knight P/Moving script.cs
@@ -1,18 +1,170 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Movingscript : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    enum States
+    {
+        Idle,
+        Walk,
+        Run,
+    };
+
+
+    States state;
+
+    Animator animator;
+    int iswalkingnoSword;
+    int isrunning;
+
+    Playercontroller input;
+
+    Vector2 currentMovemnt;
+    bool movementPressed;
+    bool runPressed;
+
+    private void Awake()
+    {
+
+        state = States.Idle;
+
+
+        input = new Playercontroller();
+
+        /*
+        input.Charactercontrols.Movement.performed += ctx =>
+        {
+            currentMovemnt = ctx.ReadValue<Vector2>();
+            movementPressed = currentMovemnt.x != 0 || currentMovemnt.y != 0;
+            print("movement pressed=" + movementPressed);
+
+        };
+        input.Charactercontrols.Run.performed += ctx => runPressed = ctx.ReadValueAsButton();
+        */
+
+    }
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
+
+        iswalkingnoSword = Animator.StringToHash("walkingnoSword");
+        isrunning = Animator.StringToHash("Running");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if( state == States.Idle )
+        {
+            Idle();
+        }
+
+        if (state == States.Run)
+        {
+            Run();
+        }
+
+        print("Current state=" + state);
+
+
+        //        Movent();
+
+        //      print("v2= " + input.Charactercontrols.Movement.ReadValue<Vector2>() );
+
+    }
+
+
+
+    void Idle()
+    {
+        animator.SetBool(iswalkingnoSword, false);
+
+        // check for controller analogue stick
+        Vector2 dir = input.Charactercontrols.Movement.ReadValue<Vector2>();
+
+        if (  dir.x != 0 || dir.y != 0 )
+        {
+            state = States.Run;
+        }
+
+        DoBlock();
+
+    }
+
+    void Run()
+    {
+        animator.SetBool(iswalkingnoSword, true);
+
+        // check for player letting go of analogue stick
+        Vector2 dir = input.Charactercontrols.Movement.ReadValue<Vector2>();
+        if( dir.x == 0 && dir.y == 0 )
+        {
+            state = States.Idle;
+        }
+
+        DoBlock();
+
+    }
+
+
+    void DoBlock()
+    {
+        // add code to perform block with L1
+
+
+    }
+
+    public void BlockEnded()
+    {
+        state = States.Idle;
+    }
+
+
+
+
+
+
+
+
+
+
+    void Movement()
+    {
+        bool walkingnoSword = animator.GetBool(iswalkingnoSword);
+        bool Running = animator.GetBool(isrunning);
+
+        if(movementPressed && !walkingnoSword)
+        {
+            animator.SetBool(iswalkingnoSword, true);
+        }
+
+        if (!movementPressed && walkingnoSword)
+        {
+            animator.SetBool(iswalkingnoSword, false);
+        }
+
+        if ((movementPressed && runPressed) && !Running)
+        {
+            animator.SetBool(isrunning, true);
+        }
+
+        if ((!movementPressed || !runPressed) && Running)
+        {
+            animator.SetBool(isrunning, false);
+        }
+
+    }
+
+    private void OnEnable()
+    {
+        input.Charactercontrols.Enable();
+    }
+
+    private void OnDisable()
+    {
+        input.Charactercontrols.Disable();
     }
 }
